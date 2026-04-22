2008c0eeb57a6d406b3090400b471be5f99b0d37
shafry00 <shafryyusuf@gmail.com>
2026-04-22 12:04:32 +0700

    Add project configuration and source files

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './index.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Portfolio from './components/Portfolio'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-brand-white">
+      <Navbar />
+      <main>
+        <section id="home">
+          <Hero />
+        </section>
+        <Services />
+        <Portfolio />
+        <About />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
